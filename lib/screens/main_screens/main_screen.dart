5adbdd47b641a3716ac41120a530659afa3c40f0
Nguyendq6889 <128410705+Nguyendq6889@users.noreply.github.com>
2024-01-29 15:17:54 +0700

    di chuyển notifications_page

--- lib/screens/main_screens/main_screen.dart
+++ lib/screens/main_screens/main_screen.dart
@@ -5,7 +5,7 @@ import '../../app_assets/app_icons.dart';
 import '../../modules/qr_code/pages/qr_code_scan_page.dart';
 import '../../modules/account/pages/account_page.dart';
 import '../../modules/home/pages/home_page.dart';
-import 'pages/notifications_page.dart';
+import '../../modules/notifications/pages/notifications_page.dart';
 import '../../modules/transactions/pages/transactions_page.dart';
 
 class MainScreen extends StatefulWidget {
